414b2bd09a81410d43e9219fb873be2df7affb98
Srinivasan <vasan4649@gmail.com>
2023-11-28 06:51:13 +0530

    R-6

---
+++ app/auth/Provider.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { SessionProvider } from 'next-auth/react'
+import React, { ReactNode } from 'react'
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <SessionProvider>{children}</SessionProvider>
+    )
+}
--- prisma/user.tsx
+++ prisma/user.tsx
@@ -5,7 +5,7 @@ export const getAllUsers = async () => {
     const users = await prisma.user.findMany({})
     return users
 }
-export const createUser = async (data1: UserPrisma) => {
+export const createUser = async (data1: any) => {
     console.log(data1, "data1")
     const users = await prisma.user.create({ data: data1 })
     return users
