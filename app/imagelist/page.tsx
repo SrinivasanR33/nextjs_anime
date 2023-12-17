4d73af432b54008639d6dcae7ac91522b92e5885
Srinivasan <vasan4649@gmail.com>
2023-12-17 21:33:19 +0530

    R-10

--- app/imagelist/page.tsx
+++ app/imagelist/page.tsx
@@ -8,6 +8,8 @@ import DownloadImage from '../component/DownloadButton'
 import { UploadIamgeList } from '@/utils/types'
 import MyImage from '../component/ImageComponent/CustomImage'
 import { payloadPaginationData } from '../commen/CommenTypeDefination'
+import { useAppDispatch } from '@/redux/hook/hook'
+import { setFullScreenState } from '@/redux/actions/masterSlice'
 
 
 
@@ -15,6 +17,7 @@ import { payloadPaginationData } from '../commen/CommenTypeDefination'
 function ImageList() {
     const arr = UploadArrayType
     const [imageList, setImageList] = useState([])
+    const dispatch = useAppDispatch()
     const [fullscreenImage, setFullscreenImage] = useState<string | null>(null);
     const handelFilter = async (e: React.ChangeEvent<HTMLSelectElement>) => {
         const payload: payloadPaginationData = { type: e.target.value, page: 1, pageSize: 5 }
@@ -26,6 +29,7 @@ function ImageList() {
         const element = document.documentElement;
 
         if (element.requestFullscreen) {
+            dispatch(setFullScreenState(true))
             element.requestFullscreen();
         }
         // else if (element.mozRequestFulscreen) {
@@ -39,9 +43,11 @@ function ImageList() {
 
     const closeFullscreen = () => {
         setFullscreenImage(null);
-
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
+        var doc = window.document;
+        if (doc.fullscreenElement) {
+            // Exit fullscreen
+            dispatch(setFullScreenState(false))
+            doc.exitFullscreen();
         }
         // else if (document.mozCancelFullScreen) {
         //     document.mozCancelFullScreen();
@@ -58,50 +64,52 @@ function ImageList() {
         };
     };
     return (
-        <div>
-
-            <div className='flex gap-7 p-2'>
-                {/* {folderNameandId} */}
+        <>
+            {!fullscreenImage ? <>
                 <div>
-                    <select className="select select-accent w-full max-w-xs" onChange={handelFilter}>
-                        <option value={""}>Select Folder name</option>
-                        {arr.map((val, i) => (
-                            <option key={i} value={val.name}>{val.name}</option>
-                        ))}
+                    <div className='flex gap-7 p-2'>
+                        {/* {folderNameandId} */}
+                        <div>
+                            <select className="select select-accent w-full max-w-xs" onChange={handelFilter}>
+                                <option value={""}>Select Folder name</option>
+                                {arr.map((val, i) => (
+                                    <option key={i} value={val.name}>{val.name}</option>
+                                ))}
 
-                    </select>
-                </div>
-                {/* <div>
+                            </select>
+                        </div>
+                        {/* <div>
                 <button className='btn btn-accent btn-sm'>filter</button>
             </div> */}
-            </div>
-            <div className='grid lg:grid-cols-6 md:grid-cols-8 sm:grid-cols-10 xs:grid-cols-12'>
-                {imageList.map((val: UploadIamgeList, i) => (
-                    <div key={i} className='p-2'>
-                        <DownloadImage publicId={val.publicId} />
-                        <MyImage src={val.secureUrl}
-                            width={500} // Set your desired width
-                            height={300} // Set your desired height
-                            priority={true}
-                            quality={85}
-                            onClick={() => openFullscreen(val.publicId)}
-                            alt='image' />
                     </div>
-                ))}
-            </div>
-            {fullscreenImage && (
-                <div className='fullscreen-overlay' onClick={closeFullscreen}>
-                    <MyImage
-                        src={fullscreenImage}
-                        width={getScreenDimensions().width}
-                        height={getScreenDimensions().height}
-                        priority={true}
-                        quality={85}
-                        alt='fullscreen-image'
-                    />
+                    <div className='grid lg:grid-cols-6 md:grid-cols-8 sm:grid-cols-10 xs:grid-cols-12'>
+                        {imageList.map((val: UploadIamgeList, i) => (
+                            <div key={i} className='p-2'>
+                                <DownloadImage publicId={val.publicId} />
+                                <MyImage src={val.secureUrl}
+                                    width={500} // Set your desired width
+                                    height={300} // Set your desired height
+                                    priority={true}
+                                    quality={85}
+                                    onClick={() => openFullscreen(val.secureUrl)}
+                                    alt='image' />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            )}
-        </div>
+            </> : <><div className='fullscreen-overlay' onClick={closeFullscreen}>
+                <MyImage
+                    src={fullscreenImage}
+                    width={getScreenDimensions().width}
+                    height={getScreenDimensions().height}
+                    priority={true}
+                    quality={85}
+                    alt='fullscreen-image'
+                />
+            </div></>
+            }
+        </>
+
 
     )
 }
