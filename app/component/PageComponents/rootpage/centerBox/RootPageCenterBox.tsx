6dc47ed775efb49c1c356982b3413a9a70643e8f
Srinivasan <vasan4649@gmail.com>
2024-01-11 22:50:09 +0530

    Scroll package

--- app/component/PageComponents/rootpage/centerBox/RootPageCenterBox.tsx
+++ app/component/PageComponents/rootpage/centerBox/RootPageCenterBox.tsx
@@ -1,15 +1,27 @@
 "use client"
 // CenterBox.tsx
 import styles from './styles.module.css';
-
+import { motion } from "framer-motion";
 const CenterBox: React.FC = () => {
 
 
     return (
         <div className={styles.container}>
-            <div className={`${styles.box} ${styles.box1}`}>
+            <motion.div
+                style={{ width: 200, height: 200 ,background:"gray"}}
+                initial={{ scale: 0 ,rotate:0}}
+                animate={{
+                    rotate: "180deg",
+                    scale:2
+                }}
+                transition={{
+                    duration: 1,
+                    ease: 'backIn'
+                    
+                }}
+            >
                 Box 1
-            </div>
+            </motion.div>
             <div className={`${styles.box} ${styles.box2}`}>
                 Box 2
             </div>
